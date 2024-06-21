64ba651069fdccf1b6605c2e1822f12c24ed4565
nc-c0001 <nc-c0001@ncubed.nl>
2024-06-21 13:13:25 +0000

    Add parsing code

---
+++ app/parsing/drivers/tests/parsing/test_ttp.py
@@ -0,0 +1,3 @@
+# TODO: i would like the ttp parte to be tested, every sub template should be tested indevidually like in teh example below
+
+# result = ios.parse(os.path.join(BACKUP_FOLDER), os.path.join(DESTINATION_FOLDER,HOSTNAME+'.oc'), template = 'show_run_access_list.ttp')
