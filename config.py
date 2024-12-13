3634fa8967e8115994ffb7d4e7cc274275f98f56
nc-c0001 <nc-c0001@ncubed.nl>
2024-12-13 08:29:43 +0000

    Edit jwks_uri

--- config.py
+++ config.py
@@ -134,6 +134,7 @@ OAUTH_PROVIDERS = [{
             'request_token_url': None,
             'client_id': f'{AUTHENTIK_CLIENT_ID}',
             'client_secret': f'{AUTHENTIK_CLIENT_SECRET}',
+            'jwks_uri': f'{AUTHENTIK_DOMAIN}/application/o/fab-data-visualiser/jwks/'
         }
    },
        {
